456fe2fe50d7d6a27faa5f908ef111ee7eb96f92
LAXMI KUMAR SAHANI <85158969+sahanilxm@users.noreply.github.com>
2023-01-05 01:31:28 +0530

    Update button.component.ts

--- src/app/event-page/event-card/button/button.component.ts
+++ src/app/event-page/event-card/button/button.component.ts
@@ -5,7 +5,7 @@ import { Component, OnInit, Input } from '@angular/core';
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.css']
 })
-export class ButtonComponent implements OnInit{
+export class ButtonComponent implements OnInit {
   @Input() value!: string;
   @Input() link!: string;
   constructor() { }
