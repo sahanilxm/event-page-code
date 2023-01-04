73d37d9bd0c36073905297c86f82726d5b2fb4ee
LAXMI KUMAR SAHANI <85158969+sahanilxm@users.noreply.github.com>
2023-01-05 00:34:50 +0530

    Update main.component.ts

--- src/app/event-page/main/main.component.ts
+++ src/app/event-page/main/main.component.ts
@@ -1,4 +1,4 @@
-import { Input, Component, OnInit, } from '@angular/core';
+import { Component, OnInit, } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { catchError, map } from 'rxjs/operators';
 import { throwError, Observable } from 'rxjs';
