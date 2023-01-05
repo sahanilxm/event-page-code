cc8a54dcb61e374bb792adc6f418f14a09cf2dd2
LAXMI KUMAR SAHANI <85158969+sahanilxm@users.noreply.github.com>
2023-01-05 12:09:09 +0530

    Update tags.component.ts

--- src/app/event-page/tags/tags.component.ts
+++ src/app/event-page/tags/tags.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-tags',
