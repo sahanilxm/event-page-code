b2dcbbadbd2e8b10e267799a6f4db2ffd35001b9
LAXMI KUMAR SAHANI <85158969+sahanilxm@users.noreply.github.com>
2023-01-05 14:49:40 +0530

    Add files via upload

---
+++ src/app/event-page/event-card/event-registered-users/event-registered-users.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+
+@Component({
+  selector: 'app-event-registered-users',
+  templateUrl: './event-registered-users.component.html',
+  styleUrls: ['./event-registered-users.component.css']
+})
+export class EventRegisteredUsersComponent {
+  @Input() imgSrc!: string;
+  @Input() name!: string;
+  @Input() otherUsers!: string;
+  @Input() regStatus!: string;
+  constructor() { }
+
+  ngOnInit(): void {
+    // console.log(this.name);
+    // console.log(this.imgSrc);
+  }
+}
